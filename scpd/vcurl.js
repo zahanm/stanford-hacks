cb8ffe19455d365478ef2c9ba4376fcf49c0043e
Zahan Malkani <zahanm@gmail.com>
2012-10-28 15:25:26 -0700

    started the script, using zombie

--- scpd/vcurl.js
+++ scpd/vcurl.js
@@ -1,9 +1,52 @@
 
+/*
+Deps
+*/
+
 var vcurl = require('commander');
+var Browser = require('zombie');
+var assert = require('assert');
+
+/*
+Globals
+*/
+
+var browser = new Browser();
+var expectedFileSize = 444448768;
+
+/*
+Command-line specs
+*/
 
 vcurl
   .version('0.0.1')
-  .option('-v, --video-url', 'HTTP Video URL')
+  .option('-c, --class', 'Class name')
   .parse(process.agrv);
 
-console.log("hello");
+/*
+Script
+*/
+
+function errorOut(err) {
+  console.error(err);
+  process.exit(1);
+}
+
+function main() {
+  vcurl.class = vcurl.class.toLowerCase();
+  browser.on("error", errorOut);
+  browser.visit("https://myvideosu.stanford.edu")
+  .then(function () {
+    debugger;
+  })
+}
+
+/*
+Boilerplate
+*/
+
+if (require.main === module) {
+  main();
+} else {
+  console.log("This script is meant for interactive use only");
+}
